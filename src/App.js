c0da2fbe173db9bc458ee527b430248ee4f3f47f
jis2831 <jis2831@naver.com>
2020-11-23 10:49:06 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">   
-        <img src={logo} className="App-logo" alt="logo" />
-          <h2>
-            Let's develp management system!
-          </h2>
-    </div>
-  );
+const customer = {
+'id': 1,
+'image': 'https://placeimg.com/64/64/any',
+'name': '홍길동',
+'birthday': '961222',
+'gender': '남자',
+'job': '대학생'
+}
+
+class App extends Component {
+render() {
+return (
+<Customer
+id={customer.id}
+image={customer.image}
+name={customer.name}
+birthday={customer.birthday}
+gender={customer.gender}
+job={customer.job}
+/>
+);
+}
 }
 
 export default App;
+
